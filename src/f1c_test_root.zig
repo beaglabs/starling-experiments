bca741859e0ace0d068c915eb82463e0597e63fe
Comrade Lemoncake <jdbohrman@outlook.com>
2026-08-29 17:36:04 -0400

    f1c: add zquic test root

---
+++ src/f1c_test_root.zig
@@ -0,0 +1,9 @@
+const wire = @import("finalization/f1c_wire.zig");
+const runtime = @import("finalization/f1c_zquic_runtime.zig");
+const experiment = @import("finalization/f1c_zquic_experiment.zig");
+
+test {
+    _ = wire;
+    _ = runtime;
+    _ = experiment;
+}
