78e52eb94e725bfea91237fb957e18313ab0f0d4
Comrade Lemoncake <jdbohrman@outlook.com>
2026-08-29 07:57:14 -0400

    f3: add inference-control test root

---
+++ src/f3_test_root.zig
@@ -0,0 +1,9 @@
+const policy = @import("finalization/f3_inference_policy.zig");
+const search = @import("finalization/f3_search.zig");
+const reference = @import("finalization/f3_stage7b_reference.zig");
+
+test {
+    _ = policy;
+    _ = search;
+    _ = reference;
+}
