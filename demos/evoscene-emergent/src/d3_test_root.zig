ae19353b14014541b90244010f4b655645389b55
Comrade Lemoncake <jdbohrman@outlook.com>
2026-08-30 00:04:42 -0400

    Add D3 population tests

---
+++ demos/evoscene-emergent/src/d3_test_root.zig
@@ -0,0 +1,19 @@
+const observation = @import("d3_observation.zig");
+const population = @import("d3_population.zig");
+const spatial_prior = @import("agents/spatial_prior.zig");
+const geometry = @import("agents/geometry.zig");
+const view_planner = @import("agents/view_planner.zig");
+const novel_view = @import("agents/novel_view.zig");
+const fusion = @import("agents/fusion.zig");
+const critic = @import("agents/critic.zig");
+
+test {
+    _ = observation;
+    _ = population;
+    _ = spatial_prior;
+    _ = geometry;
+    _ = view_planner;
+    _ = novel_view;
+    _ = fusion;
+    _ = critic;
+}
